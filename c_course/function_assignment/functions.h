09d6a255c73ece356dd0676603bbc20ed75b4f50
mostafa mohamed elsayed elsyed <mostafa.emary255@gmail.com>
2022-07-24 20:50:26 +0200

    function

---
+++ c_course/function_assignment/functions.h
@@ -0,0 +1,21 @@
+/*
+ * functions.h
+ *
+ *  Created on: Sep 26, 2021
+ *      Author: Mostafa
+ */
+#include "stdio.h"
+#include"stdlib.h"
+#include "string.h"
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+#endif /* FUNCTIONS_H_ */
+int calcminimum(int arr[],int size);
+void findnames(char name[],char names[][20],int i);
+void diss (int num);
+int power(int,int);
+int factorial(int);
+void check_prime_number(int star,int end);
+void practice_recursion(int num);
+void reverse(void);
+
